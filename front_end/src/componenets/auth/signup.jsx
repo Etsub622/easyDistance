cbd5ffa18dd3d37ecad9ae650a2f7073c1308015
Rediet Haileab <wegenerediet@gmail.com>
2024-08-29 01:26:28 +0300

    login and singup

---
+++ front_end/src/componenets/auth/signup.jsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import { MdVisibility, MdVisibilityOff } from 'react-icons/md';
+import { Link } from "react-router-dom";
+import pic from "../../assets/search.png"
+import hat from "../../assets/graduate.png"
+
+const Signup = () => {
+    // const [email, setEmail] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("")
+    const [collegename,setCollegname]=useState("")
+    const [password, setPassword] = useState("");
+    const[confirmedpassword,setConfirmedPassword]=useState()
+    const [showPassword, setShowpassword] = useState(false)
+    
+    const togglevisibility = () => {
+        setShowpassword(!showPassword)
+    }
+
+    return (
+        <div className=" flex flex-col justify-end items-center m-9">
+            <div>
+            <div className=" flex justify-center gap-4 mb-6">
+                    <img src={hat} alt="graduate hat " className="w-20 -rotate-45" />
+                <p className="font-poppins text-36 font-black pt-3"> sign up Today!</p>
+                </div>
+                <div className=" flex justify-center gap-2 w-96 p-2 pl-2 border rounded-lg focus:outline-none focus:border-gray-500 mb-5">
+                    <img src={pic} alt="google logo " className="w-6"  />
+                    <p className="text-customColor-2 font-bold font-epilogue"> Sign Up with Google </p>
+                </div>
+                <div className="flex items-center w-full mb-4">
+                  <div className="flex-grow border-t border-gray-300"></div>
+                   <span className="px-4 text-gray-700">Or Sign Up with Email</span>
+                  <div className="flex-grow border-t border-gray-300"></div>
+                  </div>
+
+                <div className="">
+                      <div> 
+                        <label className="block font-epilogue text-customColor-1 font-medium
+                          ">First Name</label>
+                        <input
+                            className=" w-96 p-2 pl-2 border rounded-lg focus:outline-none focus:border-gray-500 mb-3"
+                            type="text"
+                            placeholder="Enter your first name"
+                            value={firstname}
+                            onChange={(e) => setFirstname(e.target.value)}
+                        />
+                    </div>
+                      <div> 
+                        <label className="block font-epilogue  text-customColor-1 font-medium">Last Name</label>
+                        <input
+                            className=" w-96 p-2 pl-2 border rounded-lg focus:outline-none focus:border-gray-500 mb-3"
+                            type="text"
+                            placeholder="Enter your last name"
+                            value={lastname}
+                            onChange={(e) => setLastname(e.target.value)}
+                        />
+                    </div>
+                      <div> 
+                        <label className="block font-epilogue  text-customColor-1 font-medium">College</label>
+                        <input
+                            className=" w-96 p-2 pl-2 border rounded-lg focus:outline-none focus:border-gray-500 mb-3 "
+                            type="text"
+                            placeholder="Enter your college name"
+                            value={collegename}
+                            onChange={(e) => setCollegname(e.target.value)}
+                        />
+                    </div>
+                     <div >
+                        <label className="block font-epilogue  text-customColor-1 font-medium">Password</label>
+                        <div  className="relative">
+                        <input
+                            className="p-2 pl-2 border rounded-lg w-96 mb-3" 
+                            type={showPassword ? "text":"password"}
+                            placeholder="Enter password"
+                            value={password}
+                             onChange={(e) => setPassword(e.target.value)}
+                        />
+                          <button
+                             type="button"
+                              className="absolute top-3 -translate-x-7 text-gray-500"
+                              onClick={togglevisibility}
+            >
+                                 {showPassword ? <MdVisibility /> : <MdVisibilityOff />}
+                            </button>
+                            </div>
+                    </div>
+                     <div >
+                        <label className="block font-epilogue  text-customColor-1 font-medium "> Confirm Password</label>
+                        <div  className="relative">
+                        <input
+                            className="p-2 pl-2 border rounded-lg w-96"
+                            type={showPassword ? "text":"password"}
+                            placeholder="Enter password"
+                            value={confirmedpassword}
+                             onChange={(e) => setConfirmedPassword(e.target.value)}
+                        />
+                          <button
+                             type="button"
+                              className="absolute top-3 -translate-x-7 text-gray-500"
+                              onClick={togglevisibility}
+            >
+                                 {showPassword ? <MdVisibility /> : <MdVisibilityOff />}
+                            </button>
+                            </div>
+                    </div>
+                        <div className="flex justify-center mt-5"> 
+                    <button className="bg-customColor-0 w-96 p-2 pl-2 border  rounded-lg text-white text-28">
+                        Continue
+                    </button>
+                    </div>
+                      <div className="flex gap-2 mt-3">
+                        <p className="text-gray-500">Already have an account?</p>
+                        <Link to="/" className="text-customColor-0">Login</Link>
+                    </div>
+                    
+
+                </div>
+                </div>
+        </div>
+       
+    );
+}
+
+export default Signup;
--- front_end/tailwind.config.js
+++ front_end/tailwind.config.js
@@ -2,10 +2,25 @@
 export default {
   content: [
       "./src/**/*.{js,jsx,ts,tsx}", 
-      "./public/index.html", 
+      "./index.html", 
   ],
   theme: {
-    extend: {},
+     fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      epilogue: ['Epilogue'],
+      langar:['Langar']
+      },
+      fontSize: {
+        '48': '48px',
+        '28': '20px',
+        '36':'36px'
+      },
+    extend: {
+      
+ colors: {
+        customColor: ['#43C5A4' ,'#515B6F','#12C698'],
+      },
+    },
   },
   plugins: [],
 }
